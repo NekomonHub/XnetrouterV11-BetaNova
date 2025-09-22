aac4746fa5d384d3186779dfe7c531d61ff8dc4a
ЯpЛ <zennloveit@gmail.com>
2025-09-22 14:30:52 +0700

    Add files via upload

---
+++ plugin/run.js
@@ -0,0 +1,35 @@
+const c = require('chalk');
+const log = console.log;
+
+function ban(){
+const teks = `  ............''',,;;;::ccllodkO0K0kxdoollcc::;;,,'''..
+............'''',,;;;::cclodo::clccoO0Odollcc::;;,,'''.
+...........''',,,;;::ccllod;.........,oKkdolcc::;;,,,''
+..........''',,;;;::ccllod,....  .   ..lKxoolcc::;;,,,'
+:,''....''',,,;;;::ccllod,.        ..  cKOdoolcc::;;,,'
+doc;,;,,,,,,;;;:cccllooo;.        ... .:kkddoolcc::;;,,
+xkxo:::::::::ccccllloooc.        .;;'',;xxxddoolcc::;;,
+dxxdlcldxkOkxxddlloooo:..       .::clx;dOkkxdddoolcc:::
+loddddxkO00KK0Okdoddc...      ..'dkkd;od0OOOkkxxdollllo
+cllddddxkkxdodxkOd:....       .';:c0d:kk0OOOOOOkxolooxO
+;;;;;;;;;:lxxoc;.....        ...'';cXx00OKKK00Okdooodxx
+;;cclccc:,;,'.....          .....'':c0XKKXKKK00Okxxxkkd
+::::;,.'''....     .'...  ... ...'::cdXXNXK000OOOOO0KKO
+,;;'..'....    ..;:c.............'',:cxKXK0OOOkkOOKXK0k
+,........   .,lkk:.. ............'',;:cld00OkkkO0KXKK0O
+... .... 'cdkko,..;,   .''.......',''',,cdKXK0KKKXK0Okk
+..  .:'.:xdd:....oc,'.  .,;'...........',;:lk0XKKK0OOkx
+....:c,,loo'.:;.o;,...';;..'.     ...',,''',;lkX00Okxdd
+'...'..coc..dc.,;,'..';kNOo'...   ..'''.','':cxKKOkxxol
+;,,':l.c;.'dl..,,'..';oKNko,   .....'.';::cx;;::lo,'...
+,,cclo:,.'oo'..,'..',:dKk,...    ...'.,cc,.....',:;;cox
+'.;looo;,;c:...''''',ckx;.........',,..lOc,,;:coooolloo
+,,'ldxdl,oo;,..,,,'';oo:'.',;:cc::,'. .,:lxollcldkkxooo
+;::::ccl:od:l:,codoooodlcc:,'..         ;'dxoooodOK000O
+:ccclooolloodooddxxxd;....              .;,xdddxxO0XXXX
+ccllodxxxxxxxxdl::cc'.        .........',::okkxdkKKKKK0
+ooooolcccloddxkkkdl;... . ....'',',::ccccll:dkdxkkkkxxx
+;:;;;,,,,;cooddxxxocl:;..,::c:cc::cooccccccoxO000kxollc`
+log(c.red.bold(teks));
+}
+module.exports = ban
